b89c5102b2942cbfaf07f579533057adc6850fe7
harshsharma7895 <147915087+harshsharma7895@users.noreply.github.com>
2026-06-05 04:58:17 +0530

    Fixed backend2

--- server/controllers/renterController.js
+++ server/controllers/renterController.js
@@ -35,8 +35,8 @@ const loginRenter = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure:true,
+      sameSite:"none"
     });
 
     return res.status(200).json({
